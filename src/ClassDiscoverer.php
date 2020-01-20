5f448d05a80cd8df3e2ab1fc509d5e308460b5f7
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-01-19 22:49:40 -0300

    Refactored the class discoverer to something less tight coupled.

--- src/ClassDiscoverer.php
+++ src/ClassDiscoverer.php
@@ -7,12 +7,16 @@ use ReflectionClass;
 use ReflectionMethod;
 use ReflectionException;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use Symfony\Component\Finder\Finder;
+use Illuminate\Database\Eloquent\Collection;
+use const DIRECTORY_SEPARATOR;
 
 /**
  * Class ClassDiscoverer
  * ---
- * This is a blatant copy of the DiscoverEvents, but modified to handle only class names.
+ * This is the object-version of the DiscoverEvents class, but modified to handle only class names and filter
+ * these classes by method or implementation. It needs
  *
  * @see \Illuminate\Foundation\Events\DiscoverEvents
  * @package DarkGhostHunter\Laratraits
@@ -20,82 +24,191 @@ use Symfony\Component\Finder\Finder;
 class ClassDiscoverer
 {
     /**
-     * Get a list of the classes based on the files inside a directory.
+     * The Base Path as root namespace to look up.
      *
-     * @param  string  $path  The path to look for, relative to the $basePath.
-     * @param  string  $basePath  The base path. If null, it will use the application base path.
-     * @param  string|null  $method  Optional filter by a public method in each class.
-     * @return \Illuminate\Support\Collection
+     * @var string
      */
-    public static function from(string $path, string $basePath = null, string $method = null)
-    {
-        $basePath = $basePath ?? base_path();
+    protected $basePath;
 
-        return collect(static::getFiles(
-            (new Finder)->files()->in($path), $basePath, $method
-        ))->mapToDictionary(function ($path, $class) {
-            return [$path => $class];
-        });
+    /**
+     * Base Namespace for the classes to discover.
+     *
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     * Directory path to look inside.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Filtering mechanism
+     *
+     * @var array[]
+     * @example [ 'filter' => 'interface', 'value' => \Illuminate\Contracts\Support\Jsonable::class ]
+     */
+    protected $filter;
+
+    /**
+     * Creates a new ClassDiscoverer instance.
+     *
+     * @param  string  $basePath
+     * @param  string|null  $namespace
+     */
+    public function __construct(string $basePath = null, string $namespace = null)
+    {
+        $app = app();
+
+        $this->basePath = trim($basePath ?? $app->basePath(), DIRECTORY_SEPARATOR);
+        $this->namespace = $namespace ?? $app->getNamespace();
+        $this->path = $app->path();
     }
 
     /**
-     * Get all of the files and parse the class if it exists.
+     * Path to look for inside a directory.
      *
-     * @param  iterable  $files
-     * @param  string  $basePath
-     * @param  string|null  $methodName
-     * @return array
+     * @param  string  $path
+     * @return \DarkGhostHunter\Laratraits\ClassDiscoverer
      */
-    protected static function getFiles(iterable $files, string $basePath, string $methodName = null)
+    public function path(string $path)
     {
-        $classes = [];
+        $this->path = $this->basePath . DIRECTORY_SEPARATOR . trim($path, DIRECTORY_SEPARATOR);
 
-        foreach ($files as $file) {
-            try {
-                $class = new ReflectionClass(
-                    static::classFromFile($file, $basePath)
-                );
-            }
-            catch (ReflectionException $e) {
-                continue;
-            }
+        return $this;
+    }
 
-            if (! $class->isInstantiable()) {
-                continue;
-            }
+    /**
+     * Filters the class list by only those containing a public method.
+     *
+     * @param  string  $method
+     * @return \DarkGhostHunter\Laratraits\ClassDiscoverer
+     */
+    public function filterByMethod(string $method)
+    {
+        $this->filter = [
+            'type'  => 'method',
+            'value' => $method,
+        ];
 
-            if (! $methodName) {
-                $classes[] = $class->name;
-                continue;
-            }
+        return $this;
+    }
 
-            foreach ($class->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-                if (! Str::is($methodName . '*', $method->name)) {
-                    continue;
-                }
-
-                $classes[] = $class->name;
-            }
+    /**
+     * Filter the class list by only those implementing a given interface.
+     *
+     * @param  string  $interface
+     * @return $this
+     */
+    public function filterByInterface(string $interface)
+    {
+        if (! interface_exists($interface)) {
+            throw new InvalidArgumentException("The interface [$interface] has not been declared.");
         }
 
-        return $classes;
+        $this->filter = [
+            'type'  => 'interface',
+            'value' => $interface,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Returns the Class names.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function discover()
+    {
+        return collect($this->getFiles())
+            ->filter([$this, 'filterClasses'])
+            ->when($this->filter, function (Collection $classes, $filter) {
+                return $classes->filter([
+                    $this, $filter['type'] === 'interface' ? 'filterInterface' : 'filterMethod'
+                ]);
+            });
+    }
+
+    /**
+     * Get the files for the given path
+     *
+     * @return \Symfony\Component\Finder\Finder
+     */
+    protected function getFiles()
+    {
+        return (new Finder)->files()->in($this->path);
     }
 
     /**
      * Extract the class name from the given file path.
      *
      * @param  \SplFileInfo  $file
-     * @param  string  $basePath
      * @return string
      */
-    protected static function classFromFile(SplFileInfo $file, $basePath)
+    protected function classFromFile(SplFileInfo $file)
     {
-        $class = trim(Str::replaceFirst($basePath, '', $file->getRealPath()), DIRECTORY_SEPARATOR);
+        $class = trim(Str::replaceFirst($this->basePath, '', $file->getRealPath()), DIRECTORY_SEPARATOR);
 
         return str_replace(
-            [DIRECTORY_SEPARATOR, ucfirst(basename(app()->path())) . '\\'],
-            ['\\', app()->getNamespace()],
+            [DIRECTORY_SEPARATOR, ucfirst(basename($this->path)) . '\\'],
+            ['\\', $this->namespace],
             ucfirst(Str::replaceLast('.php', '', $class))
         );
+    }
+
+    /**
+     * Returns a Reflection Class from the SplFileInfo File
+     *
+     * @param  \SplFileInfo  $file
+     * @return null|\ReflectionClass
+     */
+    protected function filterClasses(SplFileInfo $file)
+    {
+        try {
+            $class = new ReflectionClass($this->classFromFile($file));
+        } catch (ReflectionException $e) {
+            return null;
+        }
+
+        if (! $class->isInstantiable()) {
+            return null;
+        }
+
+        return $class;
+    }
+
+    /**
+     * Filter each class by a method name.
+     *
+     * @param  \ReflectionClass  $class
+     * @return null|\ReflectionClass
+     */
+    protected function filterMethod(ReflectionClass $class)
+    {
+        foreach ($class->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            if (Str::is($this->filter['value'] . '*', $method->name)) {
+                return $class;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Filter each class by a given implementation.
+     *
+     * @param  \ReflectionClass  $class
+     * @return null|\ReflectionClass
+     */
+    protected function filterInterface(ReflectionClass $class)
+    {
+        if (in_array($this->filter['value'], $class->getInterfaceNames(), true)) {
+            return $class;
+        }
+
+        return null;
     }
 }
--- src/DiscoverClasses.php
+++ src/DiscoverClasses.php
@@ -7,7 +7,7 @@ namespace DarkGhostHunter\Laratraits;
  * ---
  * This class allows to spy inside a directory and check all PHP files containing classes a-la PSR-4.
  * You will receive a Collection made of all the instantiable classes, optionally filtered by a
- * method name (if the method is not present, the class is discarded) to tidy up the list.
+ * method name or implementation interface. You will receive a collection of the classes.
  *
  * @package DarkGhostHunter\Laratraits
  */
@@ -16,12 +16,22 @@ trait DiscoverClasses
     /**
      * Discover instantiable classes from a given path.
      *
-     * @param  string  $path  The Path to discover
-     * @param  string  $method  The method name to filter the classes
-     * @return \Illuminate\Support\Collection  A collection of class names.
+     * @param  string  $path  The Path to discover. Defaults to the
+     * @param  string|null  $methodOrInterface The Method name or Interface to filter the classes.
+     * @return \Illuminate\Support\Collection  A collection of filtered class names.
      */
-    protected function discover(string $path, string $method = null)
+    protected function discover(string $path, string $methodOrInterface = null)
     {
-        return ClassDiscoverer::from($path, base_path(), $method);
+        $discoverer = (new ClassDiscoverer())->path($path);
+
+        if ($methodOrInterface) {
+            if (interface_exists($methodOrInterface)) {
+                $discoverer->filterByInterface($methodOrInterface);
+            } else {
+                $discoverer->filterByMethod($methodOrInterface);
+            }
+        }
+
+        return $discoverer->discover();
     }
 }
