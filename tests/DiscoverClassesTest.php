deef478fb8221681cd9c40854faf5e3ee6f1fc11
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-04-28 15:04:20 -0400

    Revert "Added fix for github actions (realpaths)."
    This reverts commit 85aba4f6

--- tests/DiscoverClassesTest.php
+++ tests/DiscoverClassesTest.php
@@ -3,19 +3,21 @@
 namespace Tests;
 
 use Mockery;
+use Illuminate\Support\Str;
 use InvalidArgumentException;
 use Orchestra\Testbench\TestCase;
 use DarkGhostHunter\Laratraits\DiscoverClasses;
 use DarkGhostHunter\Laratraits\ClassDiscoverer;
 use Illuminate\Contracts\Foundation\Application;
+use const DIRECTORY_SEPARATOR as DS;
 
 class DiscoverClassesTest extends TestCase
 {
     public function testDiscoverClasses()
     {
-        if (getenv('GITHUB_ACTIONS')) {
-            return $this->markTestSkipped('Github actions does not detect the stub directory');
-        }
+//        if (getenv('GITHUB_ACTIONS')) {
+//            return $this->markTestSkipped('Github actions does not detect the stub directory');
+//        }
 
         $discovers = new class() {
             use DiscoverClasses;
@@ -24,9 +26,9 @@ class DiscoverClassesTest extends TestCase
         $app = Mockery::spy(Application::class);
 
         $app->shouldReceive('path')
-            ->andReturn(realpath(__DIR__));
+            ->andReturn(__DIR__);
         $app->shouldReceive('basePath')
-            ->andReturn(realpath(__DIR__ . '/..'));
+            ->andReturn(Str::beforeLast(__DIR__, DS));
 
         $this->app->when(ClassDiscoverer::class)
             ->needs(Application::class)
@@ -57,9 +59,9 @@ class DiscoverClassesTest extends TestCase
 
     public function testExceptionWhenInterfaceDoesntExists()
     {
-        if (getenv('GITHUB_ACTIONS')) {
-            return $this->markTestSkipped('Github actions does not detect the stub directory');
-        }
+//        if (getenv('GITHUB_ACTIONS')) {
+//            return $this->markTestSkipped('Github actions does not detect the stub directory');
+//        }
 
         $this->expectException(InvalidArgumentException::class);
         $discoverer = app(ClassDiscoverer::class)->path('tests');
